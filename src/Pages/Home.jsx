b4c4e8d02cc4340bd2ad26648b09472e468ba856
Sparsh Kumar <76520495+sparshkr24@users.noreply.github.com>
2022-05-30 16:59:04 +0530

    used API to fetch movie data and set state  variable

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
-import MainPageLayout from '../Components/MainPageLayout'
+import React, { useState } from 'react';
+import MainPageLayout from '../Components/MainPageLayout';
 
 const Home = () => {
+  // state variable for input
+  const [input, setInput] = useState('');
+
+  // handling change in input textarea
+  const handleOnChange = e => {
+    setInput(e.target.value);
+  };
+
+  // Function to handle search
+  const handleSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=girls
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(response => response.json())
+      .then(data => console.log(data));
+  };
+
+  // Function to search when enter key is pressed
+  const handleKeyDown = event => {
+    if (event.which === 13) {
+      handleSearch();
+    }
+  };
+
   return (
     <MainPageLayout>
       This is Home
+      <input
+        type="text"
+        onChange={handleOnChange}
+        onKeyDown={handleKeyDown}
+        value={input}
+      />
+      <button type="submit" onClick={handleSearch}>
+        Search
+      </button>
     </MainPageLayout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
