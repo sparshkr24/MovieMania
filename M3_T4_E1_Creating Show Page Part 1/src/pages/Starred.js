6ba5b1f60e188b73798551f628ef25ca2164da1b
Sparsh Kumar <76520495+sparshkr24@users.noreply.github.com>
2022-06-02 23:18:11 +0530

    added styled Cards for actors and shows

--- M3_T4_E1_Creating Show Page Part 1/src/pages/Starred.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MainPageLayout from '../components/MainPageLayout';
-
-const Starred = () => {
-  return <MainPageLayout>this is starred</MainPageLayout>;
-};
-
-export default Starred;
