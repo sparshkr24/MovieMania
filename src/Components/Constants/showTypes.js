295f6de599d8d52be6a6a59b3b4e2e52b082577f
Sparsh Kumar <76520495+sparshkr24@users.noreply.github.com>
2022-06-08 01:35:03 +0530

    managed states using useReducer

---
+++ src/Components/Constants/showTypes.js
@@ -0,0 +1,2 @@
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAILED = 'FETCH_FAILED';
--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -1,26 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getApi } from '../getApi';
+import { FETCH_FAILED, FETCH_SUCCESS } from '../Components/Constants/showTypes';
+
+const initailState = {
+  show: null,
+  isLoading: true,
+  error: null
+}
+
+// eslint-disable-next-line consistent-return
+const reducer = (prevState, action)=>{
+  switch(action.type){
+    case FETCH_SUCCESS: 
+      return {isLoading: false, error: null, show: action.show};
+    
+    case FETCH_FAILED:
+      return {...prevState, isLoading: false, error: action.error};
+      
+    default:
+      break;
+  }
+}
 
 const Show = () => {
-  const [show, setShow] = useState(null);
   const { id } = useParams();
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+
+  // eslint-disable-next-line no-unused-vars
+  const [{show, error, isLoading}, dispatch] = useReducer(reducer, initailState);
+  // const [show, setShow] = useState(null);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
 
   useEffect(() => {
     let isMounted = true;
     getApi(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(result => {
         if (isMounted) {
-          setShow(result);
-          setIsLoading(false);
+          dispatch({type: FETCH_SUCCESS, show: result})
         }
       })
       .catch(err => {
         if (isMounted) {
-          setIsLoading(false);
-          setError(err.message);
+          dispatch({type: FETCH_FAILED, error: err.message});
         }
       });
 
@@ -29,7 +51,7 @@ const Show = () => {
     };
   }, [id]);
 
-  console.log(show);
+  // console.log(state);
   if (isLoading) {
     return <div>Page is Loading</div>;
   }
